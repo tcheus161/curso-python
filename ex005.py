08ace6eb334d5be109a80f3c57e35916a96c1da4
tcheus <matheuserick223@gmail.com>
2023-02-23 20:59:12 -0300

    inicial

---
+++ ex005.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite algum numero '))
+n2 = int(input('Digite algum numero'))
+s = n1*n2
+print('o valor {} multiplicado por {} e igual a {}' .format(n1, n2, s))
+
+
---
+++ ex006.py
@@ -0,0 +1,5 @@
+
+n = int(input('digite um numero:'))
+a = n - 1
+s = n + 1
+print('analisando o valor  {}, seu antecessor é {} e o seu sucessor e {}' .format(n, a, s))
