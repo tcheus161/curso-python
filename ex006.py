1e0382788dd7b6cead68b5b9419aca5836d33c46
tcheus161 <matheuserick223@gmail.com>
2023-02-23 21:41:41 -0300

    a

--- ex006.py
+++ ex006.py
@@ -2,4 +2,4 @@
 n = int(input('digite um numero:'))
 a = n - 1
 s = n + 1
-print('analisando o valor  {}, seu antecessor é {} e o seu sucessor e {}' .format(n, a, s))
+print('analisando 1 o valor  {}, seu antecessor é {} e o seu sucessor e {}' .format(n, a, s))
